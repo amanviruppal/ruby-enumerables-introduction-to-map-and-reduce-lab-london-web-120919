a29127f53dd8341ecd6e473339a1c0c91c9fbcc5
amanvir uppal <amanvir.uppal@icloud.com>
2019-10-17 07:53:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,74 @@
-# My Code here....
+def map_to_negativize(array)
+  final_array = []
+  i = 0 
+  while i < array.size do
+    final_array.push(array[i] * -1)
+    i+= 1 
+  end
+  final_array
+end
+
+def map_to_no_change(array)
+  final_array = []
+  i = 0 
+  while i < array.size do
+    final_array.push(array)
+    i+= 1 
+  end
+  final_array
+end
+
+def map_to_double(array)
+  final_array = []
+  i = 0 
+  while i < array.size do
+    final_array.push(array[i] * 2)
+    i+= 1 
+  end
+  final_array
+end
+
+def map_to_square(array)
+  final_array = []
+  i = 0 
+  while i < array.size do
+    final_array.push(array[i] * array[i])
+    i+= 1 
+  end
+  final_array
+end
+
+def reduce_to_total(array, starting_point=0)
+  total = starting_point
+  i = 0 
+  while i < array.size do
+    total += array[i]
+    i+= 1 
+  end
+  total
+end
+
+def reduce_to_all_true(array)
+  i = 0 
+  while i < array.size do
+   return true if 
+    i+= 1 
+  end
+  return true 
+end
+  
+def reduce_to_any_true(array)
+  i = 0 
+  while i < array.size do
+   return true if 
+    i+= 1 
+  end
+  return true  
+end
+  
+  
+  
+
+
+
+
